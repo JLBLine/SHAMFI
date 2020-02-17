bd2eb6fa124d37e380d72bafa11f64dd4b75f937
JLBLine <jack.l.b.line@gmail.com>
2020-02-17 15:14:19 +0800

    No markdown extension online

--- docs/conf.py
+++ docs/conf.py
@@ -30,12 +30,7 @@ release = '0.1b'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest',
-              'sphinx.ext.intersphinx', 'sphinx.ext.todo',
-              'sphinx.ext.ifconfig', 'sphinx.ext.viewcode',
-              'sphinx.ext.inheritance_diagram',
-              'sphinx.ext.autosummary','recommonmark',
-              'sphinx_markdown_builder']
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
